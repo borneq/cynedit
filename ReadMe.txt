cff023759863fda9badcfc0b87607945919011aa
Andrzej Borucki <borucki.andrzej@gmail.com>
2014-08-09 07:26:56 +0200

    first empty test program

---
+++ ReadMe.txt
@@ -0,0 +1,1 @@
+CynEdit is a free source code editing component written in C++. License permits use in any free project or commercial product.
---
+++ test.cxx
@@ -0,0 +1,33 @@
+#include <FL/Fl.H>
+#include <FL/Fl_Box.H>
+#include <FL/Fl_Double_Window.H>
+#include <FL/Fl_Menu_Bar.H>
+
+void quit_cb(Fl_Widget*, void*) {exit(0);}
+
+Fl_Menu_Item menutable[] = {
+  {"&File",0,0,0,FL_SUBMENU},
+    {"E&xit",	FL_ALT+FL_F+4, quit_cb, 0, FL_MENU_DIVIDER},
+	{0},
+};
+
+Fl_Box* create_status(Fl_Window &window)
+{
+  Fl_Box *statusbar = new Fl_Box(0,window.h()-20,window.w(),20);
+  statusbar->box(FL_FLAT_BOX);
+  statusbar->color(46);
+  statusbar->align(FL_ALIGN_LEFT|FL_ALIGN_INSIDE);
+  statusbar->copy_label("msg");
+  return statusbar;
+}
+
+int main(int argc, char** argv) {
+  Fl_Window window(300,300);
+  window.box(FL_NO_BOX);
+  Fl_Box *statusbar = create_status(window);
+  Fl_Menu_Bar menubar(0,0,window.w(),20);
+  menubar.menu(menutable);
+  window.end();
+  window.show(argc,argv);
+  return Fl::run();
+}
